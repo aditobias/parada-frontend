21971ea6795e73b5767eac23b7a4146ae35e2404
danluigitating <danluigitating@hotmail.com>
2019-10-29 08:41:10 +0800

    [TATINDA] Added QR Code on Receipt

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -19,13 +19,13 @@ const mapDispatchToProps = dispatch => ({
                 if(res.status == "200")
                 {
                     message.loading('You are logging in...',1)
-                    .then(()=> message.success('You are now loged in',1));
+                    .then(()=> message.success('You are now logged in',1));
                     dispatch({
                         type: 'LOG_IN',
                         payload: credentials
                     })  
                 }
-                else if(res.status == "400")
+                else if(res.status == "500")
                 {
                     message.error("You have entered an invalid username/password.");
                 }
