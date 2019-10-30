8d9fde9d0735b2f2af75d07d3a7da7bec8bb0eb2
TOBIAAD <adrian.tobias@oocl.com>
2019-10-30 17:07:47 +0800

    Revert "remove comment"
    This reverts commit 23893a4b930aa5d04b2ce516cc6b778c638cf9c5.

--- src/components/UserProfile/UserWrapper.js
+++ src/components/UserProfile/UserWrapper.js
@@ -2,10 +2,14 @@ import React, {Component} from 'react';
 import UserProfile from './UserProfile';
 
 export default class UserWrapper extends Component {
+    
+    // componentDidMount() {
+    //    this.props.getMyProfile(this.props.loginUser);
+    //   }
 
     render() {
         return (
-            <UserProfile userInfo={this.props}/>
-        )
-    }
+        <UserProfile userInfo={this.props}/>
+            )
+}
 }
