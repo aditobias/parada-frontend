123400e6c0a9aa783930c1780a2e158bf77e4688
TOBIAAD <adrian.tobias@oocl.com>
2019-10-30 17:08:41 +0800

    Revert "fix spacing, move inline declaration of css"
    This reverts commit 1c86043e84a889ca7aaf0c4b930be35866eb3ff1.

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Button, Card, Col, Icon, Input, message, Row} from 'antd';
+import {Row, Col, Card, Input, Icon, Button, message} from 'antd';
 import 'antd/dist/antd.css';
 import HeaderPage from '../Header/Header';
 import {connect} from "react-redux";
@@ -60,7 +60,9 @@ class UserProfile extends Component {
             disabled: true,
             userAction: "view",
         });
+
         message.success("Update Profile completed!");
+        console.log("tessssssssssssssst", this.props.editProfileDetail(this.props.userInfo.loginUser, this.state))
     };
 
     cancelProfile = () => {
@@ -77,17 +79,17 @@ class UserProfile extends Component {
     renderBtn = () => {
         const buttonsWhenViewing =
             <div>
-                <Button id={"editBtn"} onClick={this.editProfile}>
+                <Button id={"editBtn"} style={{float: "right"}} onClick={this.editProfile}>
                     Edit Profile
                 </Button>
             </div>;
 
         const buttonsWhenEditing =
             <div>
-                <Button id={"cancelBtn"} onClick={this.cancelProfile}>
+                <Button id={"cancelBtn"} style={{float: "right"}} onClick={this.cancelProfile}>
                     Cancel
                 </Button>
-                <Button id={"saveBtn"} onClick={this.saveProfile}>
+                <Button id={"saveBtn"} style={{float: "right"}} onClick={this.saveProfile}>
                     Save
                 </Button>
             </div>;
@@ -117,10 +119,11 @@ class UserProfile extends Component {
         return (
             <div>
                 <HeaderPage current="viewProfile"/>
-                <div className="center">
+                <div className="center" style={{width: "900px", background: "white", marginTop: "20px"}}>
                     <Row>
                         <Col span={10} style={{marginRight: "50px"}}>
-                            <Card style={{width: "380px", marginTop: "30px"}}
+                            <Card
+                                style={{width: "380px", marginTop: "30px"}}
                                 cover={<img alt="profile" src={this.state.pic} onError={this.onError}/>}>
                                 <button>Upload</button>
 
@@ -128,30 +131,30 @@ class UserProfile extends Component {
                         </Col>
                         <Col span={12} style={{fontcolor: "black"}}>
                             <Card bordered={true}>
-                                <p>Username
+                                <p style={{textAlign: "left"}}>Username
                                     <Input value={this.props.userInfo.loginUser} disabled/>
                                 </p>
-                                <p>First Name
+                                <p style={{textAlign: "left"}}>First Name
                                     <Input value={this.state.firstName}
                                            onChange={this.handleChangeFirstName}
                                            disabled={(this.state.disabled) ? "disabled" : ""}/>
                                 </p>
-                                <p>Last Name
+                                <p style={{textAlign: "left"}}>Last Name
                                     <Input value={this.state.lastName}
                                            onChange={this.handleChangeLastName}
                                            disabled={(this.state.disabled) ? "disabled" : ""}/>
                                 </p>
-                                <p>Mobile Number
+                                <p style={{textAlign: "left"}}>Mobile Number
                                     <Input value={this.state.mobileNumber}
                                            onChange={this.handleChangeMobileNumber}
                                            disabled={(this.state.disabled) ? "disabled" : ""}/>
                                 </p>
-                                <p>Email Address
+                                <p style={{textAlign: "left"}}>Email Address
                                     <Input value={this.state.email}
                                            onChange={this.handleChangeEmail}
                                            disabled/>
                                 </p>
-                                <p>
+                                <p style={{textAlign: "left"}}>
                                     Email Verification status<br/>
                                     {emailStatus}
                                 </p>
